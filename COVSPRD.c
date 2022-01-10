d64212fb80fa90ee6c731520bda3bef64f377318
IAmMADMAX <75090307+Nilanjit07@users.noreply.github.com>
2022-01-10 20:20:38 +0530

    Create COVSPRD.c

---
+++ COVSPRD.c
@@ -0,0 +1,25 @@
+#Problem Link : https://www.codechef.com/JAN221C/problems/COVSPRD
+#Problem Code : COVSPRD
+
+#include <stdio.h>
+int main() 
+{
+	int t,i,s=1;
+	scanf("%d",&t);
+	while(t--)
+	{
+	   int a,b,s=1;
+	   scanf("%d %d",&a,&b);
+	   for(int i=1;i<=b&&s<a;i++)
+	   {
+            if(i<11) 
+                s*=2;
+            else 
+                s*=3;
+        }
+        if(s>=a) 
+            s=a;
+        printf("%d\n",s);
+ 	}
+	return 0;
+}
